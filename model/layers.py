1c31aca1919d9655eb51558a7f898756c1f51f10
Africar-Lee <africar@126.com>
2024-06-21 16:08:45 +0800

    Add STGCN

--- model/layers.py
+++ model/layers.py
@@ -4,6 +4,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init as init
 
+import torch.optim as optim
+from torch_geometric.nn import GCNConv
+import torch_geometric.nn as pyg_nn
+import torch_geometric.utils as pyg_utils
+
 from torch.nn import Linear, Parameter
 from torch_geometric.nn import MessagePassing
 from torch_geometric.utils import add_self_loops, degree
@@ -51,3 +56,36 @@ class GCNConvLayer(MessagePassing):
 
         # Step 4: Normalize node features.
         return norm.view(-1, 1) * x_j
+    
+class TemporalConvLayer(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size):
+        super(TemporalConvLayer, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=(kernel_size[0] // 2, 0))
+
+    def forward(self, x):
+        x = x.permute(0, 3, 1, 2)  # (batch, N, T, F) -> (batch, F, T, N)
+        x = self.conv(x)
+        x = x.permute(0, 2, 3, 1)  # (batch, F, T, N) -> (batch, N, T, F)
+        return x
+
+class SpatialConvLayer(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(SpatialConvLayer, self).__init__()
+        self.gcn = GCNConv(in_channels, out_channels)
+
+    def forward(self, x, edge_index):
+        # x: (batch, N, T, F)
+        # edge_index: (2, E)
+        batch_size, num_nodes, time_steps, num_features = x.shape
+        x = x.permute(0, 2, 1, 3)  # (batch, N, T, F) -> (batch, T, N, F)
+        x = x.reshape(-1, num_nodes, num_features)  # (batch * T, N, F)
+        
+        # Flatten edge_index
+        edge_index = edge_index.repeat(time_steps * batch_size, 1)  # Repeat edge_index for each time step and batch
+        edge_index = edge_index.view(2, -1)  # (2, E * batch_size * time_steps)
+        
+        x = x.reshape(-1, num_features)  # (batch * T * N, F)
+        x = self.gcn(x, edge_index)
+        x = x.view(batch_size, time_steps, num_nodes, -1)  # (batch, T, N, F)
+        x = x.permute(0, 2, 1, 3)  # (batch, T, N, F) -> (batch, N, T, F)
+        return x
--- model/models.py
+++ model/models.py
@@ -1,5 +1,6 @@
 import torch.nn.functional as F
 from torch.nn import Module
+import torch.nn as nn
 import torch
 from model import layers
 
@@ -7,18 +8,40 @@ class GCN(Module):
     def __init__(self, num_features, num_classes):
         super(GCN, self).__init__()
         self.conv1 = layers.GCNConvLayer(num_features, 24)
-        self.conv2 = layers.GCNConvLayer(24, num_classes)
+        self.conv2 = layers.GCNConvLayer(24, 16)
+        self.fc = torch.nn.Linear(16, num_classes)
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
-        node_mask = data.node_mask
+        # node_mask = data.node_mask
 
-        if node_mask is not None:
-            x = x * node_mask.unsqueeze(-1)
+        # if node_mask is not None:
+        #     x = x * node_mask.unsqueeze(-1)
 
         x = self.conv1(x, edge_index)
         x = F.relu(x)
-        x = F.dropout(x, training=self.training)
+        # x = F.dropout(x, training=self.training)
         x = self.conv2(x, edge_index)
+        x = F.relu(x)
+        x = self.fc(x)
+        x = F.relu(x)
+        return x#F.log_softmax(x, dim=1)
+    
+class STGCN(nn.Module):
+    def __init__(self, num_nodes, in_channels, time_steps, temporal_channels, spatial_channels, kernel_size,out_channels):
+        super(STGCN, self).__init__()
+        self.num_nodes = num_nodes
+        self.time_steps = time_steps
+        self.temporal1 = layers.TemporalConvLayer(in_channels, temporal_channels, kernel_size)
+        self.spatial = layers.SpatialConvLayer(temporal_channels, spatial_channels)
+        self.temporal2 = layers.TemporalConvLayer(spatial_channels, in_channels, kernel_size)
+        self.fc = torch.nn.Linear(num_nodes * in_channels * time_steps, num_nodes * out_channels * time_steps)
 
-        return F.log_softmax(x, dim=1)
+    def forward(self, x, edge_index):
+        x = self.temporal1(x)
+        x = self.spatial(x, edge_index)
+        x = self.temporal2(x)
+        x = x.reshape(x.shape[0], -1)  # Flatten
+        x = self.fc(x)
+        x = x.reshape(x.shape[0], self.num_nodes, self.time_steps, -1)  # Reshape back to (batch, N, T, F)
+        return x
